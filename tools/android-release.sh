1c183fa3a60cf2c0198efe0bca1b6e2844f5580d
Restyled.io <commits@restyled.io>
2020-02-02 20:58:05 +0000

    Restyled by shfmt

--- tools/android-release.sh
+++ tools/android-release.sh
@@ -6,7 +6,7 @@ tools/generate-files
 
 set -e
 
-version=`git describe --long --tags | cut -f 1 -d "-"`
+version=$(git describe --long --tags | cut -f 1 -d "-")
 export ANDROID_HOME="$HOME/Library/Android/sdk"
 pushd android
 ./gradlew clean build assembleRelease bintrayUpload -Pversion="$version"
